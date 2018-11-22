df692e653feccad8fab80a830a118ea8e858a661
tangjunxing <tangjunxing@bilibili.com>
2018-11-22 21:49:43 +0800

    Mod

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@
 # tjx@20181122
 #
 
-
+realpath=$(realpath $0)
+cd $(dirname $(dirname $realpath))
+tar czvf /tmp/vim-vide.tgz $(basename $(dirname $realpath))
+qshell rput static vim-vide.tgz /tmp/vim-vide.tgz
 
